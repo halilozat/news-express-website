c3c6510f49e5d4c965a9b8fd1337ef77d9914f88
halilozat <halilozat@hotmail.com>
2021-05-13 22:35:15 +0300

    project start and blog theme

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+const hostname = '127.0.0.1'
+
+
+//public dosyalara erişmek için kullanılır.
+app.use(express.static('public'))
+
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/index.html'))
+})
+
+app.get('/about',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/about.html'))
+})
+
+app.get('/blog',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+})
+
+app.listen(port,hostname, () => {
+    console.log('Server is living!');
+} )
