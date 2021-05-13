d2c18007b1ccaa421710870d709d58041f41a341
halilozat <halilozat@hotmail.com>
2021-05-13 23:31:13 +0300

    login page, register page

--- app.js
+++ app.js
@@ -26,6 +26,22 @@ app.get('/blog',(req,res) => {
     res.render('site/blog')
 })
 
+app.get('/contact',(req,res) => {
+    res.render('site/contact')
+})
+
+app.get('/login',(req,res) => {
+    res.render('site/login')
+})
+
+app.get('/register',(req,res) => {
+    res.render('site/register')
+})
+
+
+
+
+
 app.listen(port,hostname, () => {
     console.log('Server is living!');
 } )
