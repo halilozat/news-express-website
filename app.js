89cd65631d8f0af3ee89c927fc39b47a1c143a62
halilozat <halilozat@hotmail.com>
2021-05-14 14:10:32 +0300

    CRUD operations test, routes, addPost page

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const express = require('express')
 const exphbs  = require('express-handlebars')
 const app = express()
@@ -8,7 +7,7 @@ const mongoose = require('mongoose');
 
 
 
-await mongoose.connect('mongodb://127.0.0.1/nodenews_db', {
+mongoose.connect('mongodb://127.0.0.1/nodenews_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -21,31 +20,8 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
-
-app.get('/',(req,res) => {
-    //using index handlebars...
-    res.render('site/index')
-})
-
-app.get('/about',(req,res) => {
-    res.render('site/about')
-})
-
-app.get('/blog',(req,res) => {
-    res.render('site/blog')
-})
-
-app.get('/contact',(req,res) => {
-    res.render('site/contact')
-})
-
-app.get('/login',(req,res) => {
-    res.render('site/login')
-})
-
-app.get('/register',(req,res) => {
-    res.render('site/register')
-})
+const main = require('./routes/main')
+app.use('/',main)
 
 
 
--- test.js
+++ test.js
@@ -9,9 +9,25 @@ mongoose.connect('mongodb://127.0.0.1/mongoose_test_db', {
   useCreateIndex: true
 });
 
-Post.create({
-    title: 'My first news',
-    content: 'lorem imsum news'
-}, (error,post)=>{
-    console.log(error,post);
-})
+
+// Post.find({},(error,post)=>{
+//   console.log(error, post);
+// })
+
+
+// Post.create({
+//     title: 'My second news',
+//     content: 'lorem imsum second news'
+// }, (error,post)=>{
+//     console.log(error,post);
+// })
+
+Post.findById('609e4fbc3ff0522980a347fa', (error,post)=>{
+  console.log(error,post);
+ })
+
+// Post.findByIdAndUpdate('609e4fbc3ff0522980a347fa', {
+//   title: '2. new'
+// },(error,post)=>{
+//   console.log(error,post)
+// })
