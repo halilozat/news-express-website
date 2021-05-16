5e3f8e0c4ddfa9972b5965384c88c5521acec699
halilozat <halilozat@hotmail.com>
2021-05-16 15:36:29 +0300

    Admin Panel && Add Category Operation

--- app.js
+++ app.js
@@ -82,9 +82,13 @@ app.use(bodyParser.json())
 const main = require('./routes/main')
 const posts = require('./routes/posts')
 const users = require('./routes/users')
+const admin = require('./routes/admin/index')
+
+
 app.use('/', main)
 app.use('/posts', posts)
 app.use('/users', users)
+app.use('/admin', admin)
 
 
 
---
+++ routes/admin/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const Category = require('../../models/Category')
+
+router.get('/', (req, res) => {
+    res.render('admin/index')
+})
+
+router.get('/categories', (req, res) => {
+
+    Category.find({}).lean().then(categories => {
+        res.render('admin/categories', {categories:categories})
+
+    })
+})
+
+router.post('/categories', (req, res) => {
+
+    Category.create(req.body, (error, category) => {
+        if (!error) {
+            res.redirect('categories')
+        }
+    })
+
+
+})
+
+module.exports = router
